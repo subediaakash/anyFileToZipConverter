ac354548f4fc95debc23faae1ea073f96c14c141
spartan-readytocode <7007966033abc@gmail.com>
2025-01-24 22:53:46 +0530

    feat : initialisation

---
+++ src/components/text-extractor.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TextExtractorFromPdf() {
+  return (
+    <div>
+      <h1>Text Extractor From PDF</h1>
+    </div>
+  );
+}
+
+export default TextExtractorFromPdf;
